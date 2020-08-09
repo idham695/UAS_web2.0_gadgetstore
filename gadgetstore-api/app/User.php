88a860c488486c635feb0c896abf33b1bdfc066c
nashrul48 <60307816+nashrul48@users.noreply.github.com>
2020-08-09 22:48:43 +0700

    Add files via upload

--- gadgetstore-api/app/User.php
+++ gadgetstore-api/app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -16,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','username', 'roles', 'address', 'city_id', 'province_id', 'phone', 'avatar', 'status'
     ];
 
     /**
@@ -27,6 +28,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function generateToken(){
+        $this->api_token = Str::random(60);
+        $this->save();
+        return $this->api_token;
+    }
 
     /**
      * The attributes that should be cast to native types.
