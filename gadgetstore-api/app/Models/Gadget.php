dfd44df88a9e07783c5658b283f3de0b6b34548a
nashrul48 <60307816+nashrul48@users.noreply.github.com>
2020-08-11 15:13:47 +0700

    Gadget.php

--- gadgetstore-api/app/Models/Gadget.php
+++ gadgetstore-api/app/Models/Gadget.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Gadget extends Model
 {
     protected $fillable = ['name', 'slug', 'description', 'image', 'price', 'weight', 'stock', 'status'];
-
+    
+    public function categories() {
+        return $this->belongsToMany('App\Models\Category');
+    }
 }
