d7d59259e67f91139d21f1846bee7deb8940e4d0
idham kholid <idhamkholid965@gmail.com>
2020-08-11 15:27:25 +0700

    resolve

--- gadgetstore-api/routes/api.php
+++ gadgetstore-api/routes/api.php
@@ -29,11 +29,17 @@ Route::prefix('v1')->group(function () {
 
     Route::post('login', 'AuthController@login');
     Route::post('register', 'AuthController@register');
+    Route::get('gadgets', 'GadgetController@index');
+    Route::get('gadgets/slug/{slug}', 'GadgetController@slug');
+    Route::get('my-order', 'ShopController@myOrder');
+
     Route::middleware('auth:api')->group(function () {
         Route::post('logout', 'AuthController@logout');
         Route::post('edit-profile/{id}', 'AuthController@editProfile');
     });
+
 });
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
